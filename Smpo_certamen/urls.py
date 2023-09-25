c35d240054eb63963e13e72d85c13124eb813287
simonpinilla <simon.pinilla10@gmail.com>
2023-09-25 15:17:21 -0300

    vista creadas en app1 y url añadidos

--- Smpo_certamen/urls.py
+++ Smpo_certamen/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Smpo_App1 import views as v1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app1/', v1.vista1),
+
+
 ]
