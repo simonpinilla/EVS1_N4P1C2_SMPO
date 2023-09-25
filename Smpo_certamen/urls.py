dd52b15e72ccf6a9a16fc834c1f3a6d64aa51074
simonpinilla <simon.pinilla10@gmail.com>
2023-09-25 15:47:33 -0300

    vistas creadas en app2

--- Smpo_certamen/urls.py
+++ Smpo_certamen/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App2 import views as v2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vista1/', v2.vista1),
+    path('vista2/', v2.vista2),
+
+
+
+    
 ]
